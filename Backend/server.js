7885096fd0887e38655286e5d2d3b9fb0191225a
Anamika <anamika2004abc@gmail.com>
2026-05-15 22:50:21 +0530

    Fixed frontend CORS URL

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 9000;
 // ================= MIDDLEWARE =================
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://go-fetch-mern-e-commerce-applicatio.vercel.app/",
+  "https://go-fetch-mern-e-commerce-application.vercel.app/",
 ];
 
 app.use(
