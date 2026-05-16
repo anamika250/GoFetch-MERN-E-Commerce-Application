ce70d4db0bb8cc0d5391285d317017766cf75dba
Anamika <anamika2004abc@gmail.com>
2026-05-16 13:01:32 +0530

    Changed Gmail SMTP to port 587

--- Backend/Util/mailer.js
+++ Backend/Util/mailer.js
@@ -4,16 +4,12 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-
   port: 587,
-
   secure: false,
-
   auth: {
     user: process.env.SMTP_UNAME,
     pass: process.env.SMTP_PASS,
   },
-
   tls: {
     rejectUnauthorized: false,
   },
